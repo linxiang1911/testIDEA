b3ce3eb3d7e2c210c1ce474365a4c029b0eedb9a
linxiang1911 <812880486@qq.com>
2021-11-30 11:25:41 +0800

    添加helloworld类

---
+++ src/main/java/com/linxiang/git_test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.linxiang.git_test;
+
+/**
+ * @author
+ * @create 2021-11-30 11:24
+ **/
+public class HelloWorld {
+    public static void main(){
+        System.out.println("Hello World");
+    }
+}
